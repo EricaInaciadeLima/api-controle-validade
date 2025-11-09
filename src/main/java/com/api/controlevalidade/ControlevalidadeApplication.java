c2b539a40a7516697e777411f871c7e7cd9d8880
Erica Inacia de Lima <ericainacia27@gmail.com>
2025-11-09 18:59:45 -0300

    Primeiro commit do projeto Spring

---
+++ src/main/java/com/api/controlevalidade/ControlevalidadeApplication.java
@@ -0,0 +1,13 @@
+package com.api.controlevalidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlevalidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlevalidadeApplication.class, args);
+	}
+
+}
